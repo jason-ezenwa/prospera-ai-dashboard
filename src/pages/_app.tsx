2ae0f82139d273f7bca2b290939d2aaad577c492
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2025-06-22 22:41:27 +0100

    feat: prospera ai dashboard mock up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="dark">
+      <Head>
+        <title>
+          Prospera AI - Turn B2B Inbound Leads into Customers with AI Agents
+        </title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
